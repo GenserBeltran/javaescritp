ebb2db572896c390727729adad106af07c10660f
Daniel Beltran <genserdanie-123@hotmail.com>
2022-10-15 18:04:14 +0200

    POO en Js

--- Leccion13/Poo.js
+++ Leccion13/Poo.js
@@ -30,7 +30,7 @@ class Gerente extends Empleado {
 // Polimorfismo en JavaScrip Multiples Formas de hacer un proceso
 function imprimir(tipo) {
     console.log(tipo.obtenerDetalles());
-    if (tipo instanceof Gerente) { //Palabra instanceof en JavaScript
+    if (tipo instanceof Gerente) { //Palabra instanceof en JavaScript lo MEJOR ES COLOCAR LA CLASE DE MENOR JERARQUIA
         console.log("Es un objeto de tipo empleado")
     } else {
         console.log("Es un objeto de tipo Gerente")
