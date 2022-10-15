fea9aa78a96ec7f97fd0ca4f16c2b9261fc5f1ca
Daniel Beltran <genserdanie-123@hotmail.com>
2022-10-15 18:03:53 +0200

    POO en Js

---
+++ Leccion13/Poo.js
@@ -0,0 +1,48 @@
+//Sobreescritura en JavaScript
+"use strict"
+
+class Empleado {
+
+    constructor(nombre, sueldo) {
+        this._nombre = nombre;
+        this._sueldo = sueldo;
+
+    }
+    obtenerDetalles() {
+        return "Empleado nombre: " + this._nombre + " sueldo: " + this._sueldo;
+    }
+}
+
+class Gerente extends Empleado {
+    constructor(nombre, sueldo, departamento) {
+        super(nombre, sueldo);
+        this._departamento = departamento;
+    }
+
+    obtenerDetalles() {
+        return "Gerente Nombre: " + this._nombre + "Sueldo: " + this._sueldo + "Departamento: " + this._departamento;
+
+    }
+}
+
+
+
+// Polimorfismo en JavaScrip Multiples Formas de hacer un proceso
+function imprimir(tipo) {
+    console.log(tipo.obtenerDetalles());
+    if (tipo instanceof Gerente) { //Palabra instanceof en JavaScript
+        console.log("Es un objeto de tipo empleado")
+    } else {
+        console.log("Es un objeto de tipo Gerente")
+    }
+
+}
+
+let empleado1 = new Empleado("Pepo", "Clavelo");
+let gerente1 = new Gerente("Pepo", "Clavelo", "Sistemas");
+
+imprimir(empleado1);
+imprimir(gerente1);
+
+
+
